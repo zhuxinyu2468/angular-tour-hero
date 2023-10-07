87bfb48d3b5c6b79b0723f05f79a779226f31cbc
AzureAD\XinyuZhu <xinyu.zhu@veovo.com>
2023-10-08 00:57:01 +1300

    add navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeroService } from '../hero.service';
 import { MessageService } from '../message.service';
 import { Hero } from '../hero';
@@ -9,15 +9,9 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero?:Hero;
-
   heroes: Hero[] = []
-  constructor(private heroService:HeroService, private messageService:MessageService){}
 
-  onSelect(hero: Hero):void{
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
-  }
+  constructor(private heroService: HeroService) { }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
